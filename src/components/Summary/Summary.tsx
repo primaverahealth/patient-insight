e10de5c4aa4ccf139a541d3ca5177fbbd52daf4a
Frank Corona <frankdavid.corona@gmail.com>
2021-03-01 11:47:33 -0500

    Fetch and represent Financial summary for Patient
    Signed-off-by: Frank Corona <frankdavid.corona@gmail.com>

--- src/components/Summary/Summary.tsx
+++ src/components/Summary/Summary.tsx
@@ -11,13 +11,14 @@ import {
     TableHead,
     TableRow
 } from '@material-ui/core';
+import { isNil, mapValues } from 'lodash';
 // https://www.npmjs.com/package/react-number-format
 import NumberFormat from 'react-number-format';
 
 import Divider from '../../common/Divider/Divider';
 import { width_100 } from '../../utils/WidthUtils';
 import { useAppState } from '../../state';
-import { ComponentsProps } from '../../interfaces';
+import { ComponentsProps, FinancialMemberResponse } from '../../interfaces';
 
 const useStyles = makeStyles(() => ({
     box: {
@@ -38,18 +39,23 @@ const useStyles = makeStyles(() => ({
 export default function Summary(props: ComponentsProps): ReactElement {
     const classes = useStyles();
     // prepare to use AppState
-    const { fetchPivot } = useAppState();
+    const { fetchMemberFinancial } = useAppState();
     // hook for default state of the query params to use
     const [query] = useState({ ...props.query });
     const [isFetching, setIsFetching] = useState(false);
+    const [dataSource, setDataSource] = useState([]);
 
-    // get data using the AppState
+    /**
+     * @description fetch data using the AppState
+     * @author Frank Corona Prendes <frank.corona@primavera.care>
+     */
     const fetchData = () => {
         setIsFetching(true);
-        fetchPivot(query, props.header)
-            .then(response => {
+        fetchMemberFinancial(query, props.header)
+            .then((response: any) => {
                 setIsFetching(false);
-                console.log(response);
+                // @ts-ignore
+                setDataSource(mappedInformation([response.data]));
             });
     };
 
@@ -58,13 +64,24 @@ export default function Summary(props: ComponentsProps): ReactElement {
         fetchData();
     }, [query])
 
-    // Table stuffs
-    const createData = (netPremium: number, inpatient: number, outpatient: number, specialists: number, pharmacy: number, otc: number, netRevenue: number) => {
-        return { netPremium, inpatient, outpatient, specialists, pharmacy, otc, netRevenue };
+    /**
+     * @description Mapping data to represent information.
+     * @param {FinancialMemberResponse} data
+     * @author Frank Corona Prendes <frank.corona@primavera.care>
+     */
+    const mappedInformation = (data: FinancialMemberResponse[]) => {
+        return data.map(({ netPremium, inpatient, outpatient, specialist, totalRxExpenses, netRevenue, otc }) => {
+            const response = { netPremium, inpatient, outpatient, specialist, totalRxExpenses, netRevenue, otc };
+
+            return mapValues(response, (value) => {
+                if (isNil(value)) {
+                    value = 0;
+                }
+
+                return value;
+            });
+        });
     }
-    const rows = [
-        createData(7185.001, 15381, 545, 3448, 234, 0, 13911),
-    ];
 
 
     return (
@@ -73,77 +90,81 @@ export default function Summary(props: ComponentsProps): ReactElement {
                 Summary
             </Typography>
             {isFetching && <LinearProgress/>}
-            <Divider/>
-            <TableContainer>
-                <Table className={classes.table} size="medium" aria-label="a dense table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell align="right">Net Premium</TableCell>
-                            <TableCell align="right">Inpatient</TableCell>
-                            <TableCell align="right">Outpatient</TableCell>
-                            <TableCell align="right">Specialist</TableCell>
-                            <TableCell align="right">Pharmacy</TableCell>
-                            <TableCell align="right">OTC</TableCell>
-                            <TableCell align="right">Net Revenue</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {rows.map((row) => (
-                            <TableRow key={row.netRevenue}>
-                                <TableCell align="right">
-                                    <NumberFormat value={row.netPremium}
-                                                  displayType={'text'}
-                                                  thousandSeparator={true}
-                                                  decimalScale={2}
-                                                  prefix={'$'}/>
-                                </TableCell>
-                                <TableCell align="right">
-                                    <NumberFormat value={row.inpatient}
-                                                  displayType={'text'}
-                                                  thousandSeparator={true}
-                                                  decimalScale={2}
-                                                  prefix={'$'}/>
-                                </TableCell>
-                                <TableCell align="right">
-                                    <NumberFormat value={row.outpatient}
-                                                  displayType={'text'}
-                                                  thousandSeparator={true}
-                                                  decimalScale={2}
-                                                  prefix={'$'}/>
-                                </TableCell>
-                                <TableCell align="right">
-                                    <NumberFormat value={row.specialists}
-                                                  displayType={'text'}
-                                                  thousandSeparator={true}
-                                                  decimalScale={2}
-                                                  prefix={'$'}/>
-                                </TableCell>
-                                <TableCell align="right">
-                                    <NumberFormat value={row.pharmacy}
-                                                  displayType={'text'}
-                                                  thousandSeparator={true}
-                                                  decimalScale={2}
-                                                  prefix={'$'}/>
-                                </TableCell>
-                                <TableCell align="right">
-                                    <NumberFormat value={row.otc}
-                                                  displayType={'text'}
-                                                  thousandSeparator={true}
-                                                  decimalScale={2}
-                                                  prefix={'$'}/>
-                                </TableCell>
-                                <TableCell align="right">
-                                    <NumberFormat value={row.netRevenue}
-                                                  displayType={'text'}
-                                                  thousandSeparator={true}
-                                                  decimalScale={2}
-                                                  prefix={'$'}/>
-                                </TableCell>
+            {!isFetching &&
+            <>
+                <Divider/>
+                <TableContainer>
+                    <Table className={classes.table} size="medium" aria-label="a dense table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell align="right">Net Premium</TableCell>
+                                <TableCell align="right">Inpatient</TableCell>
+                                <TableCell align="right">Outpatient</TableCell>
+                                <TableCell align="right">Specialist</TableCell>
+                                <TableCell align="right">Pharmacy</TableCell>
+                                <TableCell align="right">OTC</TableCell>
+                                <TableCell align="right">Net Revenue</TableCell>
                             </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+                        </TableHead>
+                        <TableBody>
+                            {dataSource.map((row: FinancialMemberResponse) => (
+                                <TableRow key={row.inpatient}>
+                                    <TableCell align="right">
+                                        <NumberFormat value={row.netPremium}
+                                                      displayType={'text'}
+                                                      thousandSeparator={true}
+                                                      decimalScale={2}
+                                                      prefix={'$'}/>
+                                    </TableCell>
+                                    <TableCell align="right">
+                                        <NumberFormat value={row.inpatient}
+                                                      displayType={'text'}
+                                                      thousandSeparator={true}
+                                                      decimalScale={2}
+                                                      prefix={'$'}/>
+                                    </TableCell>
+                                    <TableCell align="right">
+                                        <NumberFormat value={row.outpatient}
+                                                      displayType={'text'}
+                                                      thousandSeparator={true}
+                                                      decimalScale={2}
+                                                      prefix={'$'}/>
+                                    </TableCell>
+                                    <TableCell align="right">
+                                        <NumberFormat value={row.specialist}
+                                                      displayType={'text'}
+                                                      thousandSeparator={true}
+                                                      decimalScale={2}
+                                                      prefix={'$'}/>
+                                    </TableCell>
+                                    <TableCell align="right">
+                                        <NumberFormat value={row.totalRxExpenses}
+                                                      displayType={'text'}
+                                                      thousandSeparator={true}
+                                                      decimalScale={2}
+                                                      prefix={'$'}/>
+                                    </TableCell>
+                                    <TableCell align="right">
+                                        <NumberFormat value={row.otc}
+                                                      displayType={'text'}
+                                                      thousandSeparator={true}
+                                                      decimalScale={2}
+                                                      prefix={'$'}/>
+                                    </TableCell>
+                                    <TableCell align="right">
+                                        <NumberFormat value={row.netRevenue}
+                                                      displayType={'text'}
+                                                      thousandSeparator={true}
+                                                      decimalScale={2}
+                                                      prefix={'$'}/>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </>
+            }
         </Paper>
     );
 }
