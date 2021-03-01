da1ee8a377b82143a10577462e4572993a2c3794
Frank Corona <frankdavid.corona@gmail.com>
2021-03-01 11:29:52 -0500

    Added props for Financial Member response
    Signed-off-by: Frank Corona <frankdavid.corona@gmail.com>

--- src/interfaces/index.tsx
+++ src/interfaces/index.tsx
@@ -3,3 +3,4 @@ export * from './DoubleValuesProps';
 export * from './SingleValueProps';
 export * from './FinancialFooterProps';
 export * from './ComponentsProps';
+export * from './FinancialMemberResponse';
