56b3508a23e8725bdd6c12449c4005b37be573d6
Frank Corona <frankdavid.corona@gmail.com>
2021-03-02 07:34:25 -0500

    Added API integration to GDR component
    Signed-off-by: Frank Corona <frankdavid.corona@gmail.com>

--- src/components/SectionFinancial/GDR.tsx
+++ src/components/SectionFinancial/GDR.tsx
@@ -1,16 +1,22 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useEffect, useState } from 'react';
 import { Paper } from '@material-ui/core';
 
 import { commonStyles } from '../../utils/CommonStyles';
-import { FinancialFooterProps, SingleValueProps } from '../../interfaces';
+import { FinancialFooterProps, FinancialWidgetsProps, SingleValueProps } from '../../interfaces';
 import SingleValue from '../../common/SingleValue/SingleValue';
 import FinancialFooter from '../../common/FinancialFooter/FinancialFooter';
 
 
-export default function GDR(): ReactElement {
+export default function GDR(props: { data: FinancialWidgetsProps }): ReactElement {
     const common = commonStyles();
-    const singleValueProps: SingleValueProps = { data: { value: 96.77 } }
+    const { data } = props;
+    const [gdr, setGDR] = useState(0);
+    const singleValueProps: SingleValueProps = { data: { value: gdr } }
     const footerProps: FinancialFooterProps = { data: { name: 'GDR' } };
+
+    useEffect(() => {
+        setGDR(data.gdr || 0);
+    }, [props.data])
 
     return (
         <Paper elevation={1} className={common.box}>
--- src/components/SectionFinancial/SectionFinancial.tsx
+++ src/components/SectionFinancial/SectionFinancial.tsx
@@ -70,7 +70,7 @@ export default function SectionFinancial(props: SectionFinancialProps): ReactEle
                 <div className={classes.root}>
                     <EFR data={dataSource}/>
                     <MLR data={dataSource}/>
-                    <GDR/>
+                    <GDR data={dataSource}/>
                     <Readmissions/>
                 </div>
             </div>
