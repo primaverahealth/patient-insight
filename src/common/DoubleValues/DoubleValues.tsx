d23627eb7698698732c8faefb53e41241f03a1d8
Frank Corona <frankdavid.corona@gmail.com>
2021-03-03 16:04:22 -0500

    Introduced usage for NumberFormat component to round values to 2 decimal values
    Signed-off-by: Frank Corona <frankdavid.corona@gmail.com>

--- src/common/DoubleValues/DoubleValues.tsx
+++ src/common/DoubleValues/DoubleValues.tsx
@@ -1,5 +1,6 @@
 import React, { ReactElement } from 'react';
 import { makeStyles, Typography } from '@material-ui/core';
+import NumberFormat from 'react-number-format';
 
 import { width_50 } from '../../utils/WidthUtils';
 import { DoubleValuesProps, IDoubleValues } from '../../interfaces';
@@ -35,7 +36,12 @@ export default function DoubleValues(props: DoubleValuesProps): ReactElement {
         <div className={classes.container}>
             {data.map((item: IDoubleValues) => (
                 <div className={classes.valueContainer} key={item.description}>
-                    <div className={classes.value}>{item.value}</div>
+                    <NumberFormat
+                        className={classes.value}
+                        value={item.value}
+                        displayType={'text'}
+                        thousandSeparator={true}
+                        decimalScale={2} />
                     <Typography variant={'caption'}>{item.description}</Typography>
                 </div>
             ))}
